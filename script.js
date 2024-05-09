e58427b6dc7e4e07494448bbab0e97eb174ed5ce
Ankit Tiwari <tiwari.ankit3105@gmail.com>
2024-05-10 01:31:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "696f64c2b4msha6465b5673de12cp128e0ejsn3c425561c80b",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const getWeather = (city) => {
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      temp.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+
+      // Convert Unix timestamps to milliseconds and create Date objects
+      let sunriseTime = new Date(response.sunrise * 1000);
+      let formattedDate = sunriseTime.toLocaleDateString();
+      let formattedTime = sunriseTime.toLocaleTimeString();
+      let formattedDateTime = `${formattedDate} ${formattedTime}`;
+      sunrise.innerHTML = formattedDateTime;
+
+      let sunsetTime = new Date(response.sunset * 1000);
+      formattedDate = sunsetTime.toLocaleDateString();
+      formattedTime = sunsetTime.toLocaleTimeString();
+      formattedDateTime = `${formattedDate} ${formattedTime}`;
+      sunset.innerHTML = formattedDateTime;
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather("Delhi");
